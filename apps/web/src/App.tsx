306ed51a7555a0ce2a1a9fcb476ef716c0dfbf96
EaseAge Developer <developer@easeage.com>
2026-06-06 00:36:01 +0800

    feat: update App with layout routing

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,10 +1,14 @@
 import { Routes, Route, Navigate } from 'react-router-dom';
+import AppLayout from './components/layout/AppLayout';
+import DashboardPage from './pages/DashboardPage';
 
 export default function App() {
   return (
     <Routes>
-      <Route path="/" element={<div>EaseAge Home</div>} />
-      <Route path="*" element={<Navigate to="/" replace />} />
+      <Route path="/" element={<AppLayout />}>
+        <Route index element={<DashboardPage />} />
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Route>
     </Routes>
   );
 }
